98fd7661ea34436cd57c4e24326aee244f2f2653
quochuydev <quochuy.dev@gmail.com>
2020-01-17 15:11:10 +0700

    UPDATE

--- test.js
+++ test.js
@@ -1,11 +1,8 @@
 let APIBus = require('./index')
 
-// Port expressjs running
 const PORT = process.env.HOST || 5000;
-// Ngrok listen port 5000
 const app_host = 'https://2143d9ae.ngrok.io';
 const pathHook = `${app_host}/webhook`;
-// Your woocommerce site
 const wp_host = 'http://localhost:8080/QH1901';
 
 let API = new APIBus({ app: { wp_host, app_host, app_name: 'MYAPP', return_url: 'return_url', callback_url: 'callback_url' } });
